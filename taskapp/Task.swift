386548fd6a4c2a05ef6dc06f4a2d2f67fa94ba66
corentytakeshi <takerudrums@gmail.com>
2021-07-15 19:19:42 +0900

    課題の追加

---
+++ taskapp/Task.swift
@@ -0,0 +1,30 @@
+//
+//  Task.swift
+//  taskapp
+//
+//  Created by Takeru on 2021/07/11.
+//
+
+import RealmSwift
+
+class Task: Object {
+    // 管理用 ID。プライマリーキー
+    @objc dynamic var id = 0
+
+    // タイトル
+    @objc dynamic var title = ""
+
+    // 内容
+    @objc dynamic var contents = ""
+    
+    // カテゴリー
+    @objc dynamic var category:String = ""
+
+    // 日時
+    @objc dynamic var date = Date()
+
+    // id をプライマリーキーとして設定
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
